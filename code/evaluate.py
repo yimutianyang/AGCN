f53fdc97933aee11a0302dc7a47538d178390861
yimutianyang <40798740+yimutianyang@users.noreply.github.com>
2020-08-24 16:17:29 +0800

    Update evaluate.py

--- code/evaluate.py
+++ code/evaluate.py
@@ -97,3 +97,114 @@ def get_metrics_test(user_matrix, item_matrix, test_ratings, all_ratings, itemse
         ndcg_out[topk] = round(sum(all_ndcg_list[topk])/user_matrix.shape[0], 4)
         print('topk:', topk, 'hr:', hr_out[topk], 'ndcg:',ndcg_out[topk])
     return hr_out, ndcg_out
+
+
+import pandas as pd
+from collections import defaultdict
+import numpy as np
+import random
+def read_data(path):
+    ratings = pd.read_csv(path+'ratings.csv')
+    movies = pd.read_csv(path+'movies.csv')
+    movie_key = list(movies['movieId'].values)
+    movie_id = list(range(27278))
+    movie_dict = dict(zip(movie_key, movie_id))
+    movie_features = list(movies['genres'].values)
+    ratings = ratings.values[:,:2]
+    user_items = defaultdict(set)
+    for i in range(len(ratings)):
+        user = int(ratings[i][0])-1
+        item = movie_dict[int(ratings[i][1])]
+        user_items[user].add(item)
+    return user_items, movie_features
+def encode_item(item_genres):
+    all_genres = set()
+    for i in range(len(item_genres)):
+        for j in item_genres[i].split('|'):
+            all_genres.add(j)
+    genres_dict = dict(zip(list(all_genres), list(range(len(all_genres)))))
+    item_attributes = np.zeros([item_count, len(all_genres)], dtype=np.int32)
+    for i in range(len(item_genres)):
+        for j in item_genres[i].split('|'):
+            j_index = genres_dict[j]
+            item_attributes[i][j_index] = 1
+    return item_attributes
+def split_data(rating_dict_input):
+    traindata = {}
+    valdata = {}
+    testdata = {}
+    for i in rating_dict_input.keys():
+        traindata[i] = random.sample(rating_dict_input[i],max(1,int(len(rating_dict_input[i])*0.8)))
+        val_test = rating_dict_input[i].symmetric_difference(traindata[i])
+        valdata[i] = random.sample(val_test,int(len(val_test)/2))
+        testdata[i] = list(val_test.symmetric_difference(valdata[i]))
+    return traindata,valdata,testdata
+def get_train_adj_matrix(train_rating,user_num,item_num):
+    '''
+    get train adajacent matrix
+    '''
+    item_user_train = defaultdict(set)
+    for key in train_rating.keys():
+        for i in traindata[key]:
+            item_user_train[i].add(key)
+    A_indexs = []
+    A_values = []
+    for key in train_rating.keys():
+        print(key)
+        x = key 
+        len_u = len(train_rating[key])
+        for i in range(len(train_rating[key])):
+            y = train_rating[key][i] + user_num
+            len_v = len(item_user_train[train_rating[key][i]])
+            A_indexs.append([x,y])
+            A_values.append(1/len_u)
+            A_indexs.append([y,x])
+            A_values.append(1/len_v)
+    return A_indexs, A_values
+def get_1000_negatives(rating_all, item_count, negatives_sample=1000):
+    itemset = set(range(item_count))
+    user_negatives_1000 = {}
+    for key in rating_all.keys():
+        neg_list = itemset.symmetric_difference(rating_all[key])
+        user_negatives_1000[key] = random.sample(neg_list, negatives_sample)
+    return user_negatives_1000
+def get_missing_attributes(missing_rate, item_count):
+    '''
+    根据missing_rate随机删除item attributes
+    1.初始化profiel矩阵为0
+    2.分别随机抽取存在不同profile的用户列表
+    3.根据2填充1
+    '''
+    itemset = set(range(item_count))
+    item_attributes_missing = np.zeros([item_count, 20], dtype=np.float32)
+    # padding existing profile
+    existing_item_list = random.sample(list(itemset), int(item_count*(1-missing_rate)))
+    item_attributes_missing[existing_item_list] = item_attributes[existing_item_list]
+    # padding mean value for missing profiel
+    missing_item_list = list(itemset.symmetric_difference(set(existing_item_list)))
+    mean_attributes = np.mean(item_attributes[existing_item_list], axis=0) 
+    item_attributes_missing[missing_item_list] = np.tile(mean_attributes, (len(missing_item_list),1)) 
+    return item_attributes_missing, existing_item_list
+if __name__ == 'main':
+    data_path = '../ml-20m/'
+    user_count = 138493
+    item_count = 27278
+    ### load ratings & attributes ###
+    user_items, item_genres = read_data(data_path)
+    np.save(data_path+'user_items.npy', user_items)
+    np.save(data_path+'item_genres.npy', item_genres)    
+    ### encode item attributes ###
+    item_attributes = encode_item()
+    np.save(data_path+'item_attributes.npy',item_attributes)
+    ### split train/val/test ###    
+    traindata,valdata,testdata = split_data(user_items)
+    np.save(data_path+'traindata.npy',traindata)
+    np.save(data_path+'valdata.npy',valdata)
+    np.save(data_path+'testdata.npy',testdata)    
+    ### get adjacent matrix ###
+    A_indexs, A_values = get_train_adj_matrix(traindata, user_count, item_count)
+    np.save(data_path+'A_indexs.npy',A_indexs)
+    np.save(data_path+'A_values.npy',A_values)    
+    ### get 1000 negative samples ###
+    user_negatives_1000 = get_1000_negatives(user_items)
+    np.save('user_negatives_1000.npy',user_negatives_1000)
